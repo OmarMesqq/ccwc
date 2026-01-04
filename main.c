373328fc8fb6aa813609f5575765465c5efc47d6
OmarMesqq <omarmsqt@gmail.com>
2026-01-04 09:15:54 -0300

    step 3

--- main.c
+++ main.c
@@ -1,7 +1,12 @@
 #include <stdio.h>
+#include <wctype.h>
+
+#define OUT 0   // not in a word
+#define IN 1    // inside a word
 
 static void print_bytes(FILE* f, const char* filename);
 static void print_lines(FILE* f, const char* filename);
+static void print_words(FILE* f, const char* filename);
 
 int main(int argc, char** argv) {
     
@@ -26,6 +31,10 @@ int main(int argc, char** argv) {
             if (optionsArg[1] == 'l') {
                 print_lines(f, filename);
             }
+
+            if (optionsArg[1] == 'w') {
+                print_words(f, filename);
+            }
         }
     }
 
@@ -46,4 +55,33 @@ static void print_lines(FILE* f, const char* filename) {
         if (c == '\n') ++size;
     }
     printf("%4c%d %s\n", ' ', size, filename);
+}
+
+static void print_words(FILE* f, const char* filename) {
+    unsigned char state = OUT;
+    int c = 0;
+    unsigned count = 0;
+
+    /**
+    * STATE    INPUT       ACTION
+    * IN       b           state is now OUT. old word is over, increment counter
+    * OUT      nb          state is now IN
+    */
+    while ((c = fgetc(f)) != EOF) {
+        if (state == IN && iswspace(c)) {
+            state = OUT;
+            ++count;
+        }
+        else if (state == OUT && !iswspace(c)) {
+            state = IN;
+        }
+        // if (iswspace(c)) {
+        //     state = OUT;
+        // }
+        // else if (state == OUT) {
+        //     state = IN;
+        //     ++count;
+        // }
+    }
+    printf("%3c%d %s\n", ' ', count, filename);
 }
