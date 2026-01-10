53933cc5792448b04f6609d296ec88add4de1528
OmarMesqq <omarmsqt@gmail.com>
2026-01-10 13:19:50 -0300

    refactor: :truck: replace divisor by dividend bc i got it wrong

--- utils.c
+++ utils.c
@@ -5,17 +5,17 @@
 unsigned long get_number_width(unsigned long ul) {
     unsigned long width = 0;
     unsigned long q = 0;  // quotient
-    unsigned long D = ul;  // divisor: starts as the given number
+    unsigned long d = ul;  // dividend: starts as the given number
 
     /**
      * Every whole number >= 0, has at least one digit,
      * so a `do...while` seems appropriate as 
      * the width should be 1 at the minimum.
      * 
-     * Loop termination condition is whether the divisor reaches zero.
-     * As such, right after considering a number has at least one digit (`++width`),
+     * Loop termination condition is whether the dividend reaches zero.
+     * As such, right after considering that a number has at least one digit (`++width`),
      * we get the quotient of the first or previous number by 10, and set
-     * the next divisor as it.
+     * the next dividend as it.
      * 
      * The number 10 has this cool feature that allows breaking down the number in its digits
      * as we count in decimal. To get a number's width we simply count how many iterations
@@ -24,9 +24,9 @@ unsigned long get_number_width(unsigned long ul) {
      */
     do {
         ++width;
-        q = D / 10;
-        D = q;
-    } while (D != 0);
+        q = d / 10;
+        d = q;
+    } while (d != 0);
 
     
     return width;
@@ -34,37 +34,37 @@ unsigned long get_number_width(unsigned long ul) {
 
 char* numtoi(unsigned long ul, unsigned long width) {
     unsigned long q = 0;  // quotient
-    unsigned long D = 0;  // divisor
+    unsigned long d = ul;  // dividend: starts as the given number
     unsigned long r = 0;  // remainder
 
     char* str = (char*) malloc(width * sizeof(char));
     if (!str) {
+        fprintf(stderr, "numtoi: failed to allocate string to represent number!\n");
         return NULL;
     }
 
     int i = width - 1;
-    D = ul;
 
     //TODO: stop this hack
-    if (D == 0) {
+    if (d == 0) {
         str[i] = '0';
         ++i;
         str[i] = '\0';
         return str;
     }
-    q = D / 10;
-    r = D % 10;
+    q = d / 10;
+    r = d % 10;
     while (1) {
-        if (D == 0) {
+        if (d == 0) {
             break;
         }
 
         str[i] = r + '0';
         --i;
 
-        D = q;
-        q = D / 10;
-        r = D % 10;
+        d = q;
+        q = d / 10;
+        r = d % 10;
     }
     
     str[width] = '\0';
